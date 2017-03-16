681b02f62e1884d068e93472de69b48bef1078dc
Vasyl Poklitar <vasiok26@gmail.com>
2017-03-16 23:40:09 +0200

    get token

---
+++ UploadApp/UploadApp/Extensions/DataService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+namespace UploadApp
+{
+	public class DataService
+	{
+		public static string AlbumCollectionUrl;
+		static string BaseUrl = "https://api-fotki.yandex.ru/api/users/textbook.book/";
+		static string ID = "f0afaac6ebaa43cd94fabdfb02bb65a9";
+		static string Password = "f6d1762f82704c099bbc8d9ee5e5747f";
+		static string Token = "AQAAAAAcWgBoAAQiY_2AHS7QjklnuYLfJR11FJA";
+
+		public static void token()
+		{
+			using (var client = new HttpClient())
+			{
+				string atomXml = "<entry xmlns=\"http://www.w3.org/2005/Atom/\" xmlns:f=\"yandex:fotki\">< title > Горы </ title > < summary > Фотографии природы с разных источников</ summary > </ entry > ";
+					HttpRequestMessage request = new HttpRequestMessage();
+					request.RequestUri = new Uri(AlbumCollectionUrl);
+					HttpContent content = new StringContent(atomXml);
+					request.Content = content;
+				request.Method = HttpMethod.Post;
+				request.Headers.Add("Authorization", Token);
+
+				HttpResponseMessage response = client.SendAsync(request).GetAwaiter().GetResult();
+				var content2 = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+			}
+
+		}
+
+		public static void GetServiceFile()
+		{
+			var reg = new Regex("href=.+album-list");
+			using (var client = new HttpClient())
+			{
+				HttpRequestMessage request = new HttpRequestMessage();
+				request.RequestUri = new Uri(BaseUrl);
+				request.Method = HttpMethod.Get;
+			//	request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("OAuth " + Password);
+
+				HttpResponseMessage response = client.SendAsync(request).GetAwaiter().GetResult();
+				var content = Regex.Match(response.Content.ReadAsStringAsync().GetAwaiter().GetResult(), "href=.+album-list").Value;
+				AlbumCollectionUrl = Regex.Match(content, "https.+/").Value;
+			}
+		}
+
+		public static void GetAlbumCollection()
+		{
+			using (var client = new HttpClient())
+			{
+				HttpRequestMessage request = new HttpRequestMessage();
+				request.RequestUri = new Uri(AlbumCollectionUrl);
+				request.Method = HttpMethod.Get;
+
+				HttpResponseMessage response = client.SendAsync(request).GetAwaiter().GetResult();
+				var a = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+				//var content = Regex.Match(response.Content.ReadAsStringAsync().GetAwaiter().GetResult(), "href=.+album-list").Value;
+				//AlbumCollectionUrl = Regex.Match(content, "https.+/").Value;
+			}
+		}
+
+		public static void CreateAlbum()
+		{
+			string atomXml = "<entry xmlns=\"http://www.w3.org/2005/Atom/\" xmlns:f=\"yandex:fotki\">< title > Горы </ title > < summary > Фотографии природы с разных источников</ summary > </ entry > ";
+			using (var client = new HttpClient())
+			{ 
+				HttpRequestMessage request = new HttpRequestMessage();
+				request.RequestUri = new Uri(AlbumCollectionUrl);
+				request.Method = HttpMethod.Post;
+				HttpContent content = new StringContent(atomXml);
+				request.Content = content;
+				HttpResponseMessage response = client.SendAsync(request).GetAwaiter().GetResult();
+			}
+		}
+
+	}
+}
---
+++ UploadApp/UploadApp/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UploadApp
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void closeBtn_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void uploadBtn_Click(object sender, EventArgs e)
+		{
+			DataService.GetServiceFile();
+			DataService.GetAlbumCollection();
+			DataService.token();
+			//	DataService.CreateAlbum();
+		}
+
+		private void selectBtn_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
